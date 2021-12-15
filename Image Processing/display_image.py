eb0ac3cc097ed8bc6d7c7743abeb2f2a7c0bcd00
afrahaamer <afrahahmedaamer@gmail.com>
2021-12-15 11:13:20 +0530

    Adding Images

---
+++ Image Processing/display_image.py
@@ -0,0 +1,9 @@
+#
+# Reference: https://new.pythonforengineers.com/blog/image-and-video-processing-in-python/
+# Afrah Aamer
+#
+import cv2
+import sys
+rgb_img = cv2.imread("Images//RGB_illumination.jpg")
+cv2.imshow("RGB Image",rgb_img)
+cv2.waitKey(0)
--- display_image.py
+++
@@ -1,9 +0,0 @@
-#
-# Reference: https://new.pythonforengineers.com/blog/image-and-video-processing-in-python/
-# Afrah Aamer
-#
-import cv2
-import sys
-rgb_img = cv2.imread(sys.argv[1])
-cv2.imshow("RGB Image",rgb_img)
-cv2.waitKey(0)
