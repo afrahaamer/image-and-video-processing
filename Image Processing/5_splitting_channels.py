1dadf993b84a3c2a867d1f6b7cd07ac4d3fc732c
afrahaamer <afrahahmedaamer@gmail.com>
2021-12-27 11:29:40 +0530

    channel merging

--- Image Processing/5_splitting_channels.py
+++ Image Processing/5_splitting_channels.py
@@ -2,8 +2,26 @@ import cv2
 
 img = cv2.imread("C://deets//1-1//Image and Video Processing//IVP Lab//image-and-video-processing//Images//Coloured_Circle.png")
 cv2.imshow("Original Image",img)
-b,g,r = cv2.split(img)
-cv2.imshow("Blue Part of Image",b)
-cv2.imshow("Green Part of Image",g)
-cv2.imshow("Red Part of Image",r)
+
+# Method 1
+# b,g,r = cv2.split(img)
+
+# cv2.imshow("Blue Part of Image",b)
+# cv2.imshow("Green Part of Image",g)
+# cv2.imshow("Red Part of Image",r)
+
+
+# Method 2 
+r = img.copy()
+r[:,:,0] = r[:,:,1] = 0         # Setting b & g channels to 0/black
+cv2.imshow("red",r)
+
+g = img.copy()
+g[:,:,0] = g[:,:,2] = 0         # Setting b & r channels to 0/black
+cv2.imshow("green",g)
+
+b = img.copy()
+b[:,:,1] = b[:,:,2] = 0         # Setting g & r channels to 0/black
+cv2.imshow("blue",b)
+
 cv2.waitKey(0)
--- Image Processing/6_merging_channels.py
+++ Image Processing/6_merging_channels.py
@@ -1,8 +1,24 @@
 import cv2
+import numpy as np
 
 img = cv2.imread("C://deets//1-1//Image and Video Processing//IVP Lab//image-and-video-processing//Images//Coloured_Circle.png")
 cv2.imshow("Original Image",img)
+
+# Splitting the image
 b,g,r = cv2.split(img)
-img_merged = cv2.merge([r,g,b])
+
+# Merging with Black
+k = np.zeros_like(b)   # zeroes_like() creates an empty null data structure similar to the one passed to it
+bm = cv2.merge([b,k,k]) # Merging each channel with black to give the effect of colour
+gm = cv2.merge([k,g,k])
+rm = cv2.merge([k,k,r])
+
+# Displaying splitted coloured image
+cv2.imshow("Blue Part of Image",bm)
+cv2.imshow("Green Part of Image",gm)
+cv2.imshow("Red Part of Image",rm)
+
+# Merging the splitted image
+img_merged = cv2.merge([b,g,r])
 cv2.imshow("Merged Image",img_merged)
 cv2.waitKey(0)
